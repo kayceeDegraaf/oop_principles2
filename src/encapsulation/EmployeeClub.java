14e364009a95133575f05c96069be8e44cb628b8
kayceeDegraaf <katelyndegraaf@gmail.com>
2022-04-16 12:00:54 -0500

    added company practice

--- src/encapsulation/EmployeeClub.java
+++ src/encapsulation/EmployeeClub.java
@@ -5,8 +5,20 @@ import java.util.Scanner;
 public class EmployeeClub {
     public static void main(String[] args) {
 
-        /*
+       /*
+        Write a program that asks an employee to enter their information age, address and fullName
 
+        Test Data:
+        fullName = Alex Robert
+        address = 1234 Abc St, Chicago IL 12345
+        age = 32
+
+        Print "Alex Robert is in the club" if user is from Chicago and age is more than 21
+        Print "Alex Robert is not in the club" otherwise
+
+
+        EXPECTED OUTPUT:
+        Alex Robert is in the club
          */
         Scanner inputReader = new Scanner(System.in);
         Employee emp = new Employee();
